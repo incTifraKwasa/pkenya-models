109eccd858b59b607c3b01e69cf50b89bedc4fa0
dlion4 <dantemilimo@gmail.com>
2025-09-05 23:54:39 +0300

    updated the model import resolution

---
+++ pkenya_models/apps/__init__.py
@@ -0,0 +1,21 @@
+from apps.models.engineering.architecture import Architect, QuantitySurveyor
+from apps.models.health.medics import Doctor
+from apps.models.commerce.accountant import Accountant
+from apps.models.law.advocate import Advocate
+from apps.models.health.pharmacy import Pharmacy, PharmTech
+from apps.models.engineering.engineer import Engineer
+from apps.models.health.clinicians import Clinician, Qualification
+
+
+__all__ = [
+    "Clinician",
+    "Qualification",
+    "Architect",
+    "QuantitySurveyor",
+    "Engineer",
+    "Doctor",
+    "Accountant",
+    "Advocate",
+    "Pharmacy",
+    "PharmTech",
+]
